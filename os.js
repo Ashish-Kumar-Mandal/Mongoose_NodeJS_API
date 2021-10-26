e8427ba3bd28e98b6ac124c2c531ca57ca9ffd9e
Ashish Kumar Mandal <666kmandal@gmail.com>
2021-10-26 22:15:01 +0530

    add os module

---
+++ os.js
@@ -0,0 +1,35 @@
+const os = require('os');
+
+// console.log(os.arch());
+
+// console.log(os.cpus());
+
+// console.log(os.endianness());
+
+// console.log(os.freemem()/(1024*1024*1024)+" GB RAM Free");
+
+// console.log(os.totalmem()/(1024*1024*1024)+" GB RAM");
+
+// console.log(os.getPriority());
+
+// console.log(os.homedir());
+
+// console.log(os.tmpdir());
+
+// console.log(os.hostname());
+
+// console.log(os.loadavg());
+
+// console.log(os.networkInterfaces());
+
+// console.log(os.platform());
+
+// console.log(os.release());
+
+// console.log(os.type());
+
+// console.log(os.userInfo());
+
+// console.log(os.uptime());
+
+console.log(os.version());
